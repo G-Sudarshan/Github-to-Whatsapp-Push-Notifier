fbd793791907d94849bb0b675e63284a35ae99ed
G Sudarshan <43257931+G-Sudarshan@users.noreply.github.com>
2021-09-18 23:01:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from_whatsapp_no = os.environ['from_whatsapp_no']
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
-                              body='Yay! Push event triggered in master branch',
+                              body='Code Pushed in main branch of https://github.com/G-Sudarshan/Github-to-Whatsapp-Push-Notifier',
                               from_='whatsapp:'+from_whatsapp_no,
                               to='whatsapp:'+to_whatsapp_no
                           )
