ec576bd34b1ddc0acea97408f53f450c8feb29f7
G Sudarshan <43257931+G-Sudarshan@users.noreply.github.com>
2021-09-18 22:32:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from twilio.rest import Client
 account_sid = os.environ['account_sid']
 auth_token = os.environ['auth_token']
 to_whatsapp_no = os.environ['to_whatsapp_no']
+from_whatsapp_no = os.environ['from_whatsapp_no']
 
 client = Client(account_sid, auth_token)
 
